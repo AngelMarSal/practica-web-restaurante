3e5014bcb2a5a1ce5812e9fa16794569097abf65
AngelMarSal <yelo1234567891011@gmail.com>
2025-12-09 10:10:55 +0100

    Update procesar_pedido.php

--- procesar_pedido.php
+++ procesar_pedido.php
@@ -5,7 +5,8 @@ Insertar los pedidos en la bd, envia correos de confirmacion y muestra mensajes 
 
 require_once 'database/bd.php';
 require_once 'correo.php';
-
+session_start();
+$carrito = $SESSION["carrito"];
 function insertarPedidos($CodPed, $fecha, $enviado, $peso, $restaurante) {
     $sql = '\INSERT INTO Pedidos (CodPed, fecha, enviado, peso, restaurante) VALUES (?,?,?,?,?)';
     $pdo = conexion();
